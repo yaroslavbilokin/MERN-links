3573b582dc198c867e236e01bb5144dc247d8068
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-06 23:39:50 +0300

    added mongoose User model

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,27 @@
 const { Router } = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
 const router = Router()
 
 // /api/auth/register
 router.post('/register', async (req, res) => {
     try {
         const { email, password } = req.body
+
+        const candidate = await User.findOne({ email })
+
+        if (candidate) {
+            return res.status(400).json({ message: 'User already exists...' })
+        }
+
+        const hashedPassword = bcrypt.hash(password, 12)
+
+        const user = new User({ email, password: hashedPassword })
+
+        await user.save()
+
+        res.status(201).json({ message: 'User was created!' })
+
     } catch (e) {
         res.status(500).json({ message: 'Smth went wrong...' })
     }
