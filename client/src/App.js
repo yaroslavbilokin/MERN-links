1f158e382078fad95abe43eaf063ede42bf02d7e
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-07 23:12:20 +0300

    added react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'materialize-css';
+import { useRoutes } from './routes';
 
 
 function App() {
+  const routes = useRoutes(false)
   return (
-    <div className="container">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="container">
+        {routes}
+      </div>
+    </Router>
   );
 }
 
