9e06fbe8d45a4244a2e9546230113f00b2b681e3
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-07 17:14:34 +0300

    added jwtToken, express-validator

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,35 +1,88 @@
-const { Router } = require('express')
-const bcrypt = require('bcryptjs')
-const User = require('../models/User')
-const router = Router()
+const { Router } = require('express');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const router = Router();
 
 // /api/auth/register
-router.post('/register', async (req, res) => {
-    try {
-        const { email, password } = req.body
+router.post('/register',
+    [
+        check('email', 'Incorrect email...').isEmail(),
+        check('password', 'Min length - 6 characters').isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).jsom({
+                    errors: errors.array(),
+                    message: 'Incorrect registration data'
+                })
+            }
 
-        const candidate = await User.findOne({ email })
+            const { email, password } = req.body
 
-        if (candidate) {
-            return res.status(400).json({ message: 'User already exists...' })
+            const candidate = await User.findOne({ email })
+
+            if (candidate) {
+                return res.status(400).json({ message: 'User already exists...' })
+            }
+
+            const hashedPassword = bcrypt.hash(password, 12)
+
+            const user = new User({ email, password: hashedPassword })
+
+            await user.save()
+
+            res.status(201).json({ message: 'User was created!' })
+
+        } catch (e) {
+            res.status(500).json({ message: 'Smth went wrong...' })
         }
-
-        const hashedPassword = bcrypt.hash(password, 12)
-
-        const user = new User({ email, password: hashedPassword })
-
-        await user.save()
-
-        res.status(201).json({ message: 'User was created!' })
-
-    } catch (e) {
-        res.status(500).json({ message: 'Smth went wrong...' })
-    }
-})
+    })
 
 // /api/auth/login
-router.post('/login', async (req, res) => {
+router.post('/login',
+    [
+        check('email', 'Please enter correct email').normalizeEmail().isEmail(),
+        check('password', 'Please enter a password').exists()
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).jsom({
+                    errors: errors.array(),
+                    message: 'Incorrect Log In data'
+                })
+            }
+            const { email, password } = req.body
 
-})
+            const user = await User.findOne({ email })
+
+            if (!user) {
+                return res.status(400).json({ message: 'User undefined!' })
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password)
+
+            if (!isMatch) {
+                return res.status(400).json({ message: 'Incorrect password' })
+            }
+
+            const token = jwt.sign(
+                { userId: user.id },
+                config.get('jwtSecret'),
+                { expiresIn: '1h' }
+            )
+
+            res.json({ token, userId: user.id })
+
+        } catch (e) {
+            res.status(500).json({ message: 'Smth went wrong...' })
+        }
+    })
 
 module.exports = router
