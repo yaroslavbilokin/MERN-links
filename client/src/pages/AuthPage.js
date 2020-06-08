ac81d74198860f70574f45e793d2d50ee8e20e80
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-08 16:28:41 +0300

    completed authorization

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHttp } from '../hooks/http.hook';
+import { useMessage } from '../hooks/message.hook';
 
 export const AuthPage = () => {
-    const { loading, request } = useHttp();
+    const message = useMessage()
+    const { loading, request, error, clearError } = useHttp();
 
     const [form, setForm] = useState({
         email: '',
         password: ''
     })
+
+    useEffect(() => {
+        message(error);
+        clearError();
+    }, [error, message, clearError])
 
     const changeHandler = event => {
         setForm({
@@ -19,7 +26,16 @@ export const AuthPage = () => {
     const registerHandler = async () => {
         try {
             const data = await request('/api/auth/register', 'POST', { ...form });
-            console.log('Data', data)
+            message(data.message);
+        } catch (e) {
+
+        }
+    }
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', { ...form });
+            message(data.message);
         } catch (e) {
 
         }
@@ -63,6 +79,7 @@ export const AuthPage = () => {
                         <button
                             className="btn yellow darken-4"
                             style={{ marginRight: 10 }}
+                            onClick={loginHandler}
                             disabled={loading}
                         >
                             Log In
@@ -73,7 +90,7 @@ export const AuthPage = () => {
                             onClick={registerHandler}
                             disabled={loading}
                         >
-                            Sign Up
+                            Registration
                         </button>
                     </div>
                 </div>
