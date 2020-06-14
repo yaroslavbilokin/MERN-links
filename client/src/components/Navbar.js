c1b83729a7200108544b5c532d87ec84b7b6c3dc
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-14 13:14:22 +0300

    added Links back-end

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,9 +13,9 @@ export const Navbar = () => {
 
     return (
         <nav>
-            <div class="nav-wrapper blue darken-1" style={{ padding: '0 2rem' }}>
-                <a href="/" class="brand-logo">Logo</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <div className="nav-wrapper blue darken-1" style={{ padding: '0 2rem' }}>
+                <a href="/" className="brand-logo">Logo</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><NavLink to="/create">Create</NavLink></li>
                     <li><NavLink to="/links">Links</NavLink></li>
                     <li><a href="/" onClick={logoutHandler}>Logout</a></li>
--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 export const CreatePage = () => {
     return (
-        <div>
+        <div className="row">
+            <div className="col s8 offset-s2">
+
+            </div>
             <h1>Create page</h1>
         </div>
     )
