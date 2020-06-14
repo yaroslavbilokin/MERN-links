70510589b812afc41aadd93ed6be38ae615615e9
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-14 19:29:21 +0300

    added LinksList

--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useContext, useCallback, useEffect } from 'react';
+import { useHttp } from '../hooks/http.hook';
+import { AuthContext } from '../context/AuthContext';
+import { Loader } from '../components/Loader';
+import { LinksList } from '../components/LinksList';
 
 export const LinksPage = () => {
+    const [links, setLinks] = useState([]);
+    const { loading, request } = useHttp();
+    const { token } = useContext(AuthContext);
+
+    const fetchLinks = useCallback(async () => {
+        try {
+            const fetched = await request('/api/link', 'GET', null, {
+                Authorization: `Bearer ${token}`
+            })
+            setLinks(fetched);
+
+        } catch (e) { }
+    }, [token, request]);
+
+    useEffect(() => {
+        fetchLinks();
+    }, [fetchLinks])
+
+    if (loading) {
+        return <Loader />
+    }
+
+    console.log(links)
+
     return (
-        <div>
-            <h1>Links page</h1>
-        </div>
+        <>
+            {!loading && <LinksList links={links} />}
+        </>
     )
 }
